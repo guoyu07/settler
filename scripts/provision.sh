bea250c3aa40801194a7ad1b854ad5d0b05991d2
John P. Bloch <johnpbloch@gmail.com>
2016-07-27 22:05:57 -0500

    Fix issues with postgresql installation
    It wasn't starting automatically and I was getting errors regarding being unable to access `/root`, so this sets up the rc.d defaults, starts the service (to be safe) and then temporarily switches to `/tmp` so we don't get permission errors.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -212,11 +212,15 @@ apt-get install -y postgresql-9.5 postgresql-contrib-9.5
 
 # Configure Postgres Remote Access
 
+update-rc.d postgresql default
+service postgresql start
+pushd /tmp
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
 echo "host    all             all             10.0.2.2/32               md5" | tee -a /etc/postgresql/9.5/main/pg_hba.conf
 sudo -u postgres psql -c "CREATE ROLE homestead LOGIN UNENCRYPTED PASSWORD 'secret' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 sudo -u postgres /usr/bin/createdb --echo --owner=homestead homestead
 service postgresql restart
+popd
 
 # Install Blackfire
 
